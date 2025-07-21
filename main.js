70da2ed856c9c2c29b11969e1ae87ad40319d6fa
rad-ian <janek.radlinski@gmail.com>
2025-07-21 21:30:48 +0200

    BFS

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const GRID_WIDTH = 25;
 const GRID_HEIGHT = 25;
 
-const START_CELL_X = 0;
-const START_CELL_Y = 0;
-const END_CELL_X = 24;
+const START_CELL_X = 12;
+const START_CELL_Y = 12;
+const END_CELL_X = 4;
 const END_CELL_Y = 24;
 
 window.onload = () => {
@@ -12,9 +12,12 @@ window.onload = () => {
     setupContainer();
 
     const startButton = document.querySelector("#startButton");
-    // TODO: handle reset click
+    
+    const resetButton = document.querySelector("#resetButton");
 
     startButton.addEventListener("click", handleStartButtonClick);
+    
+    resetButton.addEventListener("click", handleResetButtonClick);
 };
 
 function setupContainer() {
@@ -28,8 +31,13 @@ function setupContainer() {
 
             div.classList.add("cell");
 
-            // TODO: mark starting cell
-            // TODO: mark end cell
+            if(i === START_CELL_Y && j === START_CELL_X){
+                div.classList.add("start");
+            }
+
+            if(i === END_CELL_Y && j === END_CELL_X){
+                div.classList.add("end");
+            }
 
             mazeContainer.appendChild(div);
         }
@@ -45,7 +53,18 @@ async function handleStartButtonClick() {
     const grid = createGrid();
     console.log(grid);
 
-    await visitGrid(grid);
+    await visitGridBfs(grid);
+}
+
+async function handleResetButtonClick() {
+    console.log("reset click");
+    const mazeContainer = document.querySelector("#mazeContainer");
+
+    const cells = mazeContainer.childNodes;
+
+    cells.forEach((cell) => {
+        cell.classList.remove("visited");
+    });
 }
 
 function createGrid() {
@@ -85,4 +104,55 @@ async function sleep(timeMs) {
     return new Promise((resolve) => {
         setTimeout(resolve, timeMs);
     });
+}
+
+async function visitGridBfs(grid) {
+    const queue = []
+    const graph = []
+
+    for (let i = 0; i < GRID_HEIGHT; i++) {
+        const row = []
+        for (let j = 0; j < GRID_WIDTH; j++) {
+            const cell = grid[i][j];
+            row.push({y:i , x:j , cell:cell , visited:false})
+        }
+        graph.push(row)
+    }
+
+    const startNode = graph[START_CELL_Y][START_CELL_X]
+    queue.push(startNode)
+    startNode.visited = true
+
+    while(queue.length > 0){
+        await sleep(5)
+
+        const node = queue.shift()
+        
+        node.cell.classList.add("visited");
+
+        if(node.x === END_CELL_X && node.y === END_CELL_Y){
+            console.log("hura")
+            return
+        }
+
+        if(node.y < GRID_HEIGHT - 1 && graph[node.y + 1][node.x].visited === false){
+            queue.push(graph[node.y + 1][node.x])
+            graph[node.y + 1][node.x].visited = true
+        }
+        
+        if(node.x < GRID_WIDTH - 1 && graph[node.y][node.x + 1].visited === false){
+            queue.push(graph[node.y][node.x + 1])
+            graph[node.y][node.x + 1].visited = true
+        }
+
+        if(node.y > 0 && graph[node.y - 1][node.x].visited === false){
+            queue.push(graph[node.y - 1][node.x])
+            graph[node.y - 1][node.x].visited = true
+        }
+
+        if(node.x > 0 && graph[node.y][node.x -1].visited === false){
+            queue.push(graph[node.y][node.x - 1])
+            graph[node.y][node.x - 1].visited = true
+        }
+    }
 }
